0a78db7b81c3632d95ade0cf2d5bdf088d32b444
Mario Villamil <MAVIL177@ulaval.ca>
2025-01-01 18:54:44 -0500

    Feature: User class added

--- app/models/User.py
+++ app/models/User.py
@@ -1,7 +1,7 @@
 import uuid
 
 class User:
-    def __int__(self,name, weight, height, gender, target_weight = None):
+    def __init__(self, name, weight, height, gender, target_weight=None):
         self.id = str(uuid.uuid4())
         self.name = name
         self.weight = weight
@@ -10,15 +10,15 @@ class User:
         self.target_weight = target_weight
         self.plans = []
 
-    def add_plan(self,plan):
-        self.plans.append(plan)
-
     def update_weight(self, new_weight):
         self.weight = new_weight
 
     def update_target_weight(self, new_target_weight):
         self.target_weight = new_target_weight
 
+    def add_plan(self, plan):
+        self.plans.append(plan)
 
     def __repr__(self):
-        return f"<User(name={self.name}, weight={self.weight}, height={self.height}, gender={self.gender})>"
+        return (f"<User(name={self.name}, weight={self.weight}, "
+                f"target_weight={self.target_weight}, height={self.height}, gender={self.gender})>")
